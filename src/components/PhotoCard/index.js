383cc4db10a0a2cd6cc29a0cf6dddce506d62eca
Juan C. Rodriguez W <jcrodriguezw@gmail.com>
2021-06-30 16:17:21 -0500

    haciendo un lazyloading

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,26 +1,49 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 import {
   ImgWrapper,
   Img,
-  Button
+  Button,
+  Article
 } from './styles'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1548767797-d8c844163c4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
 
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+  useEffect(() => {
+    const observer = new window.IntersectionObserver((entries) => {
+      console.log(entries)
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        console.log('si')
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+
+    observer.observe(element.current)
+  }, [element])
+
+  return (
+    <Article ref={element}>
+      {
+      show &&
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </>
+      }
+    </Article>
   )
 }

0d42a0ce650c6b58302669b05145a9f8bab4e8ce
Juan C. Rodriguez W <jcrodriguezw@gmail.com>
2021-06-30 16:31:03 -0500

    intersection-observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -15,17 +15,21 @@ export function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
   const [show, setShow] = useState(false)
 
   useEffect(() => {
-    const observer = new window.IntersectionObserver((entries) => {
-      console.log(entries)
-      const { isIntersecting } = entries[0]
-      if (isIntersecting) {
-        console.log('si')
-        setShow(true)
-        observer.disconnect()
-      }
-    })
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver((entries) => {
+        console.log(entries)
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          console.log('si')
+          setShow(true)
+          observer.disconnect()
+        }
+      })
 
-    observer.observe(element.current)
+      observer.observe(element.current)
+    })
   }, [element])
 
   return (
